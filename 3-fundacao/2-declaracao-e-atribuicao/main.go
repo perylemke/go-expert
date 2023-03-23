3f81f384477dd0657b5c4743b0df64d0219d990b
perylemketwer <pery.lemke@thoughtworks.com>
2023-03-23 16:37:01 -0300

    Second topic

---
+++ 3-fundacao/2-declaracao-e-atribuicao/main.go
@@ -0,0 +1,24 @@
+package main
+
+const a = "Hello, world!"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Pery"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X"
+	a = "XD"
+	println(a)
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+}
+
+func x() {
+
+}
