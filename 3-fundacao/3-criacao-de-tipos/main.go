2aae63cf9c76034b220343c51a635e9bb17fa442
perylemketwer <pery.lemke@thoughtworks.com>
2023-03-23 16:37:22 -0300

    Third topic

---
+++ 3-fundacao/3-criacao-de-tipos/main.go
@@ -0,0 +1,23 @@
+package main
+
+const a = "Hello, world!"
+
+type ID int
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Pery"
+	e float64 = 1.2
+	f ID      = 1
+)
+
+func main() {
+	a := "X"
+	println(a)
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+	println(f)
+}
