743e9835ce50f2873b20c0ceeb72bafed22da9c9
perylemketwer <pery.lemke@thoughtworks.com>
2023-03-31 13:55:58 -0300

    Condicionais

---
+++ 3-fundacao/24-condicionais/main.go
@@ -0,0 +1,32 @@
+package main
+
+func main() {
+	a := 1
+	b := 2
+	c := 3
+
+	if a == b {
+		println(a)
+	} else {
+		println(b)
+	}
+
+	if a > b && c > a {
+		println("a > b && c > a")
+	}
+
+	if a > b || c > a {
+		println("a > b && c > a")
+	}
+
+	switch a {
+	case 1:
+		println("a")
+	case 2:
+		println("b")
+	case 3:
+		println("c")
+	default:
+		println("d")
+	}
+}
